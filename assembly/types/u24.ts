f3b2c795bb20a396477ed2fbd2a1b129604c6994
James Brumond <james@jbrumond.me>
2020-03-13 17:14:52 -0700

    finish up ora instruction [see #2]

--- assembly/types/u24.ts
+++ assembly/types/u24.ts
@@ -9,3 +9,23 @@ export function store_u24(pointer: p, value: u32) : void {
 	store<u16>(pointer, <u16>(value & 0xffff));
 	store<u8>(pointer + 2, <u8>(value >> 16));
 }
+
+// @ts-ignore: decorator
+@inline export function u24_low_u8(value: u32) : u8 {
+	return <u8>(value & 0xff);
+}
+
+// @ts-ignore: decorator
+@inline export function u24_mid_u8(value: u32) : u8 {
+	return <u8>((value >> 8) & 0xff);
+}
+
+// @ts-ignore: decorator
+@inline export function u24_low_u16(value: u32) : u8 {
+	return <u16>(value & 0xffff);
+}
+
+// @ts-ignore: decorator
+@inline export function u24_high_u8(value: u32) : u8 {
+	return <u8>((value >> 16) & 0xff);
+}
